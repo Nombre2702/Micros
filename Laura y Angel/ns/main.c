c3942de227ebfc62a978a005c435809cc614a78f
L4g7 <lauragr.edu@gmail.com>
2025-05-07 09:23:46 +0200

    Create main.c

---
+++ Laura y Angel/ns/main.c
@@ -0,0 +1,65 @@
+#include <avr/io.h>
+#include <avr/interrupt.h>
+#include <util/delay.h>
+
+#include "funciones_lanzador.h"
+
+int main(void)
+{
+	
+	setupLanzador();
+	setupMillis();
+	
+	ceroPlataforma();
+	ledOn();
+
+	while (1)
+	{
+		
+	}
+}
+
+
+
+
+
+ISR(PCINT0_vect) {
+	lectura2 = PINB;
+	
+	 if (PINB & (1 << PB2)) sw2 = 1;
+	 if (PINB & (1 << PB3)) sw3 = 1;
+	 if (PINB & (1 << PB4)) sw4 = 1;
+	
+}
+
+void loop(){
+	antirrebotes(sw2);
+	antirrebotes(sw3);
+	antirrebotes(sw4);
+	lectura1 = lectura2;
+	
+}
+
+void antirrebotes(){
+	if (sw2) {
+		flancobajada |= (1 << SW2);
+		bloqueoentrada |= (1 << SW2);
+		rebotesw2 = 0;
+	}
+	
+	if (sw3) {
+		flancobajada |= (1 << SW3);
+		bloqueoentrada |= (1 << SW3);
+		rebotesw3 = 0;
+	}
+	
+	if (sw4) {
+		flancobajada |= (1 << SW4);
+		bloqueoentrada |= (1 << SW4);
+		rebotesw4 = 0;
+	}
+	lectura1 = lectura2;
+	
+}
+
+
