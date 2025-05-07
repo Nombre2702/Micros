5a992f0baf6af9443cb0d0299d2629656c3ceccf
MadisonBeerMaybe <aguirao61@gmail.com>
2025-05-07 14:41:43 +0200

    Add files via upload

---
+++ Laura y Angel/CompletoV5/main.c
@@ -0,0 +1,23 @@
+/*
+ * CompletoV4.c
+ *
+ * Created: 06/05/2025 23:39:23
+ * Author : ASUS
+ */ 
+
+#include "lanzador.h"
+
+
+int main(void)
+{
+    
+	setup();
+	ceroLanzador();
+	ledParpadeo();
+	
+    while (1) 
+    {
+	
+    }
+}
+
