20c6c200198a713b509c9d40286146234a4e7dc2
MadisonBeerMaybe <aguirao61@gmail.com>
2025-05-09 19:27:39 +0200

    Add files via upload

---
+++ VersionesIntegrado/IntegradoV2/integracion.h
@@ -0,0 +1,16 @@
+/*
+ * integracion.h
+ *
+ * Created: 09/05/2025 19:20:43
+ *  Author: ASUS
+ */ 
+
+
+#ifndef INTEGRACION_H_
+#define INTEGRACION_H_
+
+void timer();
+
+
+
+#endif /* INTEGRACION_H_ */
