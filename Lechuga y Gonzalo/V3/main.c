4a1bee6035cd2a35d04a673d0e7e8cde3d9badf5
Jlechuga27 <Jlechuga2701@gmail.com>
2025-05-09 14:56:15 +0200

    Update main.c

--- Lechuga y Gonzalo/V3/main.c
+++ Lechuga y Gonzalo/V3/main.c
@@ -4,6 +4,11 @@ int main(void){	//Ejemplo de uso
     setup_carga_retorno();
 	setup_rebotes();
 	puesta_cero();
+	motor_carga_status=1;	//Esto esta aqui porque si no no furula para llamar a ceros y luego seguido a las funciones, luego en el codigo no haria falta
+	while((motor_carga_status==1)||(motor_retorno_status==1)){
+			motor_carga_status = leer_motor_carga();
+		        motor_retorno_status = leer_motor_retorno();
+		}
 	ciclo_retorno();
 	ciclo_carga();
 }
